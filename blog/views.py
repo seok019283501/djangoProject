5a5ceee6d4cf3152a8612901dced8c93e7e35cd7
minseok <alsthr0338@gmail.com>
2022-12-02 20:18:52 +0900

    'video_view'

--- blog/views.py
+++ blog/views.py
@@ -3,7 +3,7 @@ from django.utils import timezone
 from .models import  Post, Video
 from .form import PostForm,VideoPost
 from django.shortcuts import redirect
-
+from embed_video.backends import detect_backend
 # Create your views here.
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
@@ -59,7 +59,7 @@ def video_new(request):
 
 def video_detail(request, video_id):
     videos = Video.objects.get(id=video_id)
-    return render(request,'video/video_detail.html',{'videos':videos})
+    return render(request,'blog/video_detail.html',{'videos':videos})
 
 
 
