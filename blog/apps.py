4169ef143a097bcd7df077b26436a1e52ee54b4a
minseok <alsthr0338@gmail.com>
2022-12-02 20:47:52 +0900

    'video_view_update'

--- blog/apps.py
+++ blog/apps.py
@@ -1,7 +1,7 @@
 import re
 
 from django.apps import AppConfig
-from  embed_video.backends import VideoBackend
+
 
 class BlogConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
